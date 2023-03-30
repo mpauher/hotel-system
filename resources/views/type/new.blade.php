665daa5b1e48e09612acd277ce79c0224c8f66d6
nessa26 <vanessamorenoarteaga@gmail.com>
2023-03-30 14:04:36 -0500

    new changes

--- resources/views/type/new.blade.php
+++ resources/views/type/new.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="bg-header-title text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Crear Tipo') }}
         </h2>
     </x-slot>
